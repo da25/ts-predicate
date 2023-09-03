df773a53328b1a2f87d6af1dc5f95d639f89f3f4
Dost Arora <dost.arora25@gmail.com>
2023-09-03 15:10:17 +0530

    Add more predicates and tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -7,10 +7,17 @@ import {
   noneItems,
   someItems,
 } from '../src/utils/array-utils.js';
-import { greaterThan, lessThan } from '../src/utils/number-utils.js';
+import { even, greaterThan, lessThan, withinBound } from '../src/utils/number-utils.js';
 import { equalTo } from '../src/utils/object-utils.js';
+import { allOf } from '../src/utils/predicate-util.js';
 
 describe('greeter function', () => {
+  let arr: number[];
+
+  beforeEach(() => {
+    arr = [2, 5, 9, 4, 7];
+  });
+
   it('test array utils', () => {
     const arr: number[] = [2, 5, 9, 4, 7];
     expect(includesItem(equalTo(1)).test(arr)).toBeFalsy();
@@ -21,5 +28,26 @@ describe('greeter function', () => {
     expect(atLeastItems(greaterThan(6), 2).test(arr)).toBeTruthy();
     expect(atMostItems(lessThan(6), 3).test(arr)).toBeTruthy();
     expect(hasSize(equalTo(5)).test(arr)).toBeTruthy();
+  });
+
+  it('should work with complex predicates', () => {
+    expect(
+      allOf([
+        everyItem(withinBound(0, 10)),
+        someItems(even())
+      ]).test(arr)
+    ).toBeTruthy();
+  });
+
+  it('should test everyItem(withinBound))', () => {
+    expect(
+      everyItem(withinBound(0, 10)).test(arr)
+    ).toBeTruthy()
+  });
+
+  it('should test someItems(even())', () => {
+    expect(
+      someItems(even())
+    ).toBeTruthy()
   });
 });
--- src/utils/number-utils.ts
+++ src/utils/number-utils.ts
@@ -1,5 +1,6 @@
 import { Predicate } from '../predicate.js';
 import { equalTo } from './object-utils.js';
+import { allOf, anyOf, not } from './predicate-util.js';
 
 export function lessThan<T extends number, U extends number>(
   comparisonValue: U,
@@ -16,22 +17,51 @@ export function greaterThan<T extends number, U extends number>(
 export function greaterThanOrEqualTo<T extends number, U extends number>(
   comparisonValue: U,
 ): Predicate<T> {
-  return greaterThan<number, number>(comparisonValue).or<number>(
-    equalTo<number>(comparisonValue),
-  );
+  return anyOf([
+    greaterThan<number, number>(comparisonValue),
+    equalTo<number>(comparisonValue)
+  ]);
 }
 
 export function lessThanOrEqualTo<T extends number, U extends number>(
   comparisonValue: U,
 ): Predicate<T> {
-  return greaterThan<number, number>(comparisonValue).or<number>(
-    equalTo<number>(comparisonValue),
-  );
+  return anyOf([
+    lessThan<number, number>(comparisonValue),
+    equalTo<number>(comparisonValue)
+  ]);
 }
 
 export function withinBound<T extends number, U extends number>(
   lowerBound: U,
   upperBound: U,
 ): Predicate<T> {
-  return greaterThanOrEqualTo(lowerBound).and(lessThanOrEqualTo(upperBound));
+  return allOf([
+    greaterThanOrEqualTo<number, number>(lowerBound),
+    lessThanOrEqualTo<number, number>(upperBound)
+  ]);
+}
+
+export function negative<T extends number>(): Predicate<T> {
+  return lessThan<T, number>(0);
+}
+
+export function positive<T extends number>(): Predicate<T> {
+  return greaterThan<T, number>(0);
+}
+
+export function divisibleBy<T extends number, U extends number>(
+  divisor: U
+): Predicate<T> {
+  return Predicate.from<T, number>((value: T) => value % divisor,
+    equalTo<number>(0)
+  );
+}
+
+export function even<T extends number>(): Predicate<T> {
+  return divisibleBy<T, number>(2);
+}
+
+export function odd<T extends number>(): Predicate<T> {
+  return not<T>(even<T>())
 }
